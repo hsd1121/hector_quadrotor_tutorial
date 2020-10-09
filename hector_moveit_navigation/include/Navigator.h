f93aced9590f0f591df1e76b83ba542612eeed4a
hsd1121 <hsd1121@vt.edu>
2020-10-09 14:27:47 -0400

    cleaned nav code

--- hector_moveit_navigation/include/Navigator.h
+++ hector_moveit_navigation/include/Navigator.h
@@ -39,17 +39,6 @@
 
 #define EPSILON 1e-4
 
-typedef std::pair<double,geometry_msgs::Pose> DistancedPoint;
-
-class Compare{
-    public: 
-        bool operator()(DistancedPoint& lhs, DistancedPoint & rhs)
-        {
-            return lhs.first < rhs.first;
-        }
-};
-typedef std::priority_queue<DistancedPoint,std::vector<DistancedPoint>, Compare> DistancedPointPriorityQueue;
-
 #include <iostream>
 #include <chrono>
 
@@ -58,7 +47,6 @@ using  ns = chrono::nanoseconds;
 using get_time = chrono::steady_clock;
 
 #include <omp.h>
-#define PATCH_LIMIT 1
 class Quadrotor{
     private:
         ros::NodeHandle nh_;
@@ -72,21 +60,15 @@ class Quadrotor{
         hector_moveit_navigation::NavigationFeedback feedback_;
         hector_moveit_navigation::NavigationResult result_;
 
-        const double takeoff_altitude = 1.0;
-        int GRID;
         bool odom_received,trajectory_received;
         bool isPathValid;
         bool collision;
 
         geometry_msgs::Pose odometry_information;
         std::vector<geometry_msgs::Pose> trajectory;
-        
-        std::vector<geometry_msgs::Pose> frontiers;
-        std::vector<geometry_msgs::Pose> explored;
 
         ros::Subscriber base_sub,plan_sub,goal_sub,distance_sub;
         ros::Publisher distance_pub;
-        ros::Publisher frontier_pub;
 
         ros::ServiceClient motor_enable_service; 
         ros::ServiceClient planning_scene_service;
@@ -94,6 +76,8 @@ class Quadrotor{
         moveit_msgs::RobotState plan_start_state;
         moveit_msgs::RobotTrajectory plan_trajectory;
     
+        bool traversing;
+
         const std::string PLANNING_GROUP = "DroneBody";
 
         void poseCallback(const nav_msgs::Odometry::ConstPtr & msg);
@@ -105,8 +89,6 @@ class Quadrotor{
         void executeCB(const hector_moveit_navigation::NavigationGoalConstPtr &goal);
 
         void computePathLengthCB(const geometry_msgs::Point::ConstPtr &path);
-
-        void findFrontier();
     
     public:
         Quadrotor(ros::NodeHandle& nh, std::string name);
